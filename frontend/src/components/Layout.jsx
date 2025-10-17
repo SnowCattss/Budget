2f765cbf86be384f04274b690b18409e907e7fbb
Snowcattss <dkemph@pm.me>
2025-10-17 10:55:46 -0400

    Fixed mistake in layout. Frontend runs and nav bar works

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,11 +2,15 @@ import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 
 export default function Layout({ children}) {
+    return (
+
     <div className="Flex min-h-screen bg-gray-100 text-gray-900">
         <Sidebar />
+
         <main className="flex-1 flex flex-col">
-            <navBar />
-            <div className=p-6>{children}</div>
+            <Navbar />
+
+            <div className="p-6">{children}</div>
         </main>
     </div>
     );
